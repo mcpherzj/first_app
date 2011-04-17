a0805b89f8226d925710193b942b980ae437d0cf
U-CATAPULT\jmcpherson <jmcpherson@CATHOUL5S4CRM1.catapultsystems.com>
2011-04-17 16:59:53 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9d5a8fac6af4201a337f6b1093842a8ccdf8ded2020b37acb33e72039186f0bbccf3ffd240371e857b182e9f7aacdb052f9a246abde2d016de77a241321a6923'
